58c98b50d24c3176a750bec44bf1391f37e036fb
amdescombes <amdescombes@gmail.com>
2016-07-08 14:38:35 +0200

    Much better handling of static and class methods

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-"""
-Creates python files for builtin modules in order to use them with PyCharm to get code completion and resolve all import issues
-"""
 import inspect
 import sound
 import _ui
 import _scene2
 
-def_fmt = '\n\n%sdef %s():\n%s    pass\n'
-method_fmt = '\n%sdef %s(self):\n%s    pass\n'
-cls_method_fmt = '\n%sdef %s(cls):\n%s    pass\n'
-class_fmt = '\n\n%sclass %s:%s\n'
-doc_fmt = '%s"""%s"""\n'
+default_indent = '    '
+def_fmt = '\n\n%sdef %s():\n'
+std_method_fmt = '\n%sdef %s(self):\n'
+cls_method_fmt = '\n%sdef %s(cls):\n'
+static_method_fmt = '\n%s@staticmethod'
+class_fmt = '\n\n%sclass %s:\n'
+doc_fmt = '    %s"""%s"""\n'
+pass_fmt = '%s    pass\n'
 
 
 def handle_attribute(name, func, indent):
@@ -20,45 +20,59 @@ def handle_attribute(name, func, indent):
         return '\n%s%s = "%s"\n' % (indent, name, func)
 
 
-def get_info(modul, indentlevel=0, inclass=False):
+def handle_function(name, func, indent, inclass):
+    isfunc = 'function' in str(func)
+    docstring = func.__doc__
+    res = []
+    if name != '__getattribute__':  # do nothing if it's a __getattribute__
+        if inspect.isclass(func):  # it is a class
+            if inclass:
+                res.append(static_method_fmt % indent)
+                res.append(std_method_fmt % (indent, name))
+            else:
+                res.append(class_fmt % (indent, name))
+        elif inclass:
+            if inspect.isbuiltin(func):  # to distiguish staticmethod and function
+                res.append(static_method_fmt % indent)
+                res.append(cls_method_fmt % (indent, name))
+            else:
+                if name in ['__format__', '__reduce_ex__', '__sizeof__', '__getslice__']:
+                    res.append(static_method_fmt % indent)
+                res.append(std_method_fmt % (indent, name))
+        else:
+            res.append(def_fmt % (indent, name))
+        if docstring:
+            res.append(doc_fmt % (indent, docstring))
+        res.append(pass_fmt % indent)
+    return res, isfunc
+
+
+def get_info(modul, indentlevel=0):
     _f = []
-    indent = '    ' * indentlevel
+    indent = default_indent * indentlevel
     for name, func in inspect.getmembers(modul):
         if callable(func):
-            if name == '__getattribute__':
-                continue
-            isfunc = 'function' in str(func)
-            if name == '__new__':
-                _f.append(cls_method_fmt % (indent, name, indent))
-            else:
-                _f.append((def_fmt if isfunc else method_fmt if inclass else class_fmt) % (indent, name, indent))
-            if not isfunc and not inclass:
-                get_info(func, indentlevel + 1, True)
+            res, isfunc = handle_function(name, func, indent, True)
+            _f += res
         else:
-            if inclass and name == '__doc__':  # insert docstring
-                _f.insert(0, doc_fmt % (indent, func))
-            else:
+            if name not in ['__doc__', '__name__', '__package__', '__dict__']:
                 _f.append(handle_attribute(name, func, indent))
     return _f
 
 
-def create_func(modul, modname, indentlevel=0, inclass=False):
+def create_func(modul, modname, indentlevel=0):
     print "processing %s" % modname
     _f = []
     indent = '    ' * indentlevel
     for name, func in inspect.getmembers(modul):
         if callable(func):
-            if name == '__getattribute__':
-                continue
-            isfunc = 'function' in str(func)
-            _f.append((def_fmt if isfunc else method_fmt if inclass else class_fmt) % (indent, name, indent))
-            if not isfunc and not inclass:
-                cls = get_info(func, indentlevel + 1, True)
+            res, isfunc = handle_function(name, func, indent, False)
+            _f += res
+            if inspect.isclass(func):
+                cls = get_info(func, indentlevel + 1)
                 _f += cls
         else:
-            if name == '__doc__':  # insert docstring
-                _f.insert(0, doc_fmt % (indent, func))
-            elif name not in ['__name__', '__package__']:
+            if name not in ['__doc__', '__name__', '__package__', '__dict__']:
                 _f.append(handle_attribute(name, func, indent))
     open(modname, 'w').write(''.join(_f))
     print "processed %s" % modname
